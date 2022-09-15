d9701cd979369fe30fe48c4a37b5c144d1d8d3a7
Sof1ane <43276871+Sof1ane@users.noreply.github.com>
2022-09-15 10:29:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ lg.info('Database import users!')
 lg.info('Database initialized!')
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='192.168.1.33', port=8000, reload=True)
+    uvicorn.run("main:app")
