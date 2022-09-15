8074643984e63c9cdbd0ca9f3f88c961077ab891
ife-oluwa <71294795+ife-oluwa@users.noreply.github.com>
2022-09-15 10:08:10 +0200

    API updates

--- App/routers/login.py
+++ App/routers/login.py
@@ -11,34 +11,38 @@ router = APIRouter(tags=['Login'], prefix="/login")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@router.post('/signup', response_model=schemas.DisplayUsers ,status_code=status.HTTP_201_CREATED)
-def create_user(request : schemas.Users, db: Session = Depends(get_db)):
+
+@router.post('/signup', response_model=schemas.DisplayUsers, status_code=status.HTTP_201_CREATED)
+def create_user(request: schemas.Users, db: Session = Depends(get_db)):
     """_summary_
        Save a json with one user 
     """
-    user = db.query(models.Users).filter(models.Users.email == request.email).first()
-    
+    user = db.query(models.Users).filter(
+        models.Users.email == request.email).first()
+
     if user:
         print(user)
         print("user already exists")
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Email already exists') 
-    
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail='Email already exists')
+
     # hashed_password = pwd_context.hash(request.hashed_password)
-    new_User = models.Users(first_name=request.first_name, last_name=request.last_name, 
+    new_User = models.Users(first_name=request.first_name, last_name=request.last_name,
                             email=request.email, hashed_password=request.hashed_password,  admin=request.admin)
     db.add(new_User)
     db.commit()
     db.refresh(new_User)
     return new_User
 
+
 @router.post('/login')
-def login(user: schemas.UserLogin, Authorize:AuthJWT=Depends(), db: Session = Depends(get_db)):
-    user_connected = db.query(models.Users).filter(models.Users.email == user.email).first()
-    if (user_connected.email==user.email) and (user_connected.hashed_password==user.hashed_password):
-        access_token= Authorize.create_access_token(subject=user.email)
+def login(user: schemas.UserLogin, Authorize: AuthJWT = Depends(), db: Session = Depends(get_db)):
+    user_connected = db.query(models.Users).filter(
+        models.Users.email == user.email).first()
+    if (user_connected.email == user.email) and (user_connected.hashed_password == user.hashed_password):
+        access_token = Authorize.create_access_token(subject=user.email)
         refresh_token = Authorize.create_refresh_token(subject=user.email)
-        return {"access token" : access_token, "refresh_token" : refresh_token}    
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Email not found / invalid user')
-
-
-    
+        first_name = user_connected.first_name
+        return {"user": first_name, "access_token": access_token, "refresh_token": refresh_token}
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                        detail='Email not found / invalid user')
--- App/routers/trainings.py
+++ App/routers/trainings.py
@@ -10,11 +10,14 @@ router = APIRouter(tags=['trainings'], prefix="/training")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@router.delete('/{id}')
-def training_delete(id, db: Session = Depends(get_db)):
-    db.query(models.Trainings).filter(models.Trainings.id == id).delete(synchronize_session=False)
+
+@router.delete('/{training_type}')
+def training_delete(training_type, db: Session = Depends(get_db)):
+    db.query(models.Trainings).filter(
+        models.Trainings.training_type == training_type).delete(synchronize_session=False)
     db.commit()
     return {'training Deleted'}
+
 
 @router.get('/')
 async def get_all_trainings(db: Session = Depends(get_db)):
@@ -23,54 +26,62 @@ async def get_all_trainings(db: Session = Depends(get_db)):
     """
     training = db.query(models.Trainings).all()
     if not training:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='training not found')
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail='training not found')
     return training
 
 
-@router.get('/{id}}')
-async def get_one_training(id, db: Session = Depends(get_db)):
+@router.get('/{training_type}}')
+async def get_one_training(training_type, db: Session = Depends(get_db)):
     """_summary_
        Get a json with one training 
     """
-    training = db.query(models.Trainings).filter(models.Trainings.id == id).first()
+    training = db.query(models.Trainings).filter(
+        models.Trainings.training_type == training_type).first()
     if not training:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='training not found')
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail='training not found')
     return training
 
-@router.get('/get_training_view/{id}}', response_model=schemas.Trainings)
+
+@router.get('/get_training_view/{training_type}}', response_model=schemas.Trainings)
 async def get_one_training_view(id, db: Session = Depends(get_db)):
     """_summary_
        Get a json with one training 
     """
-    training = db.query(models.Trainings).options(joinedload(models.Trainings.birds)).filter(models.Trainings.id == id).first()
+    training = db.query(models.Trainings).options(joinedload(
+        models.Trainings.birds)).filter(models.Trainings.id == id).first()
     if not training:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='training not found')
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail='training not found')
     return training
 
 
-@router.put('/{id}')
-def update_training(id, request : schemas.Trainings, db: Session = Depends(get_db)):
+@router.put('/{training_type}')
+def update_training(training_type, request: schemas.Trainings, db: Session = Depends(get_db)):
     """_summary_
        Update a json with one training 
     """
-    training = db.query(models.Trainings).filter(models.Trainings.id == id)
+    training = db.query(models.Trainings).filter(
+        models.Trainings.training_type == training_type)
     if not training.first():
         pass
     if not training:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail='training not found')
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail='training not found')
     training.update(request.dict())
-    db.commit()    
-    return {'bird updated'}
+    db.commit()
+    return {'Training updated'}
 
 
-@router.post('/', response_model=schemas.DisplayTrainings ,status_code=status.HTTP_201_CREATED)
-def add_training(request : schemas.Trainings, db: Session = Depends(get_db)):
+@router.post('/', response_model=schemas.DisplayTrainings, status_code=status.HTTP_201_CREATED)
+def add_training(request: schemas.Trainings, db: Session = Depends(get_db)):
     """_summary_
        Save a json with one training 
     """
-    new_training = models.Trainings(id=request.id, training_type=request.training_type, reps=request.reps, day=request.day)
+    new_training = models.Trainings(
+        id=request.id, training_type=request.training_type, reps=request.reps, day=request.day)
     db.add(new_training)
     db.commit()
     db.refresh(new_training)
     return new_training
-
